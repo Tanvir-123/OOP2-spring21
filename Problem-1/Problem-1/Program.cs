c458379b6927a7a6d484625390afad2dd388a115
Samsuzzaman Tanvir <77941228+Tanvir-123@users.noreply.github.com>
2021-02-10 00:21:31 +0600

    problem 1

---
+++ Problem-1/Problem-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, i;
+            Console.WriteLine("Enter the Countable even Number : ");
+            N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+
+            for (i = 1; i <= N; i++)
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
