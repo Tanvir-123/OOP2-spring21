843a6264889601838757ed9ac3006419547477d4
Samsuzzaman Tanvir <77941228+Tanvir-123@users.noreply.github.com>
2021-02-13 23:19:02 +0600

    2nd commit

---
+++ Problem-4/Sum-Odd and Even/Sum-Odd and Even/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Odd_and_Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1= 0,sum= 0, i, n = 50;
+            for(i=1; i<=n; i++)
+            {
+                sum += 2 * i;
+                sum1 += (2 * i) - 1;
+                Console.WriteLine("Even numbers sum is=(0)", sum);
+                Console.WriteLine("Odd numbers sum is=(0)", sum1);
+
+            }
+
+
+        }
+    }
+}
